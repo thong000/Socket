4a3a32db69d58ca787686769e75e35fbe5796113
thong000 <65811400+thong000@users.noreply.github.com>
2024-12-14 14:38:55 +0700

    Add files via upload

--- TCP/client.py
+++ TCP/client.py
@@ -1,207 +1,193 @@
 import socket
 import os
 import time
-from fileinput import filename
 from math import floor
-from multiprocessing import  Process, Pipe,Lock
-from operator import truediv
+from multiprocessing import  Process, Pipe
 
 
-def fileDataFrom(file_path, size):
+
+
+### Cac ham thao tác voi file
+def getFileData(filePath):
     try:
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(filePath, 'r', encoding='utf-8') as file:
             content = file.read()
-        if size == 0:
-            return content[0:]
-        else:
-            return content[size:]
-
+        return content
     except FileNotFoundError:
-        print(f"Tệp {file_path} không tồn tại.")
+        print(f"[INFO] Tep {filePath} khong ton tai")
         return None
 
-
-def split_string(input_string, delimiter):
-    result = input_string.split(delimiter)
+def splitString(string, delimiter):
+    result = string.split(delimiter)
+    while "" in result:
+        result.remove("")
     return result
 
-
-def getFileSize(file_path):
+def getFileSize(filePath):
     try:
-        file_size = os.path.getsize(file_path)  # Lấy kích thước tệp tính theo byte
-        return file_size
+        fileSize = os.path.getsize(filePath)
+        return fileSize
     except FileNotFoundError:
-        print(f"Tệp {file_path} không tồn tại.")
+        print(f"[INFO] Tep {filePath} khong ton tai")
         return -1
+    
+def countChangedCharacters(fileName, oldSize):
+    try:
+        curSize = getFileSize(fileName)
+        if curSize == -1:
+            return 0
+        sizeChange=curSize-oldSize
+        oldSize=curSize
+        return sizeChange
+    except FileNotFoundError:
+        return 0
 
 
 
+
+### Cac ham gui va nhan 
 def sendNumber(num, soc):
     soc.sendall(str(num).encode())
-
 def sendString(string, soc):
     soc.sendall(string.encode('utf-8'))
-
 def recvNumber(soc, size):
     return int(soc.recv(size).decode())
-
 def recvString(soc, size):
     return soc.recv(size).decode('utf-8')
-
 def recvByte(soc, size):
     return soc.recv(size)
 
-def isChange(fileName, oldSize):
-    curSize = getFileSize(fileName)
-    if curSize == None:
-        return False
-    return curSize != oldSize
 
-def receiveChunk(pipe, connection, chunkSize, part, fileName,processPipe):
+
+def receiveChunk(dataPipe, connection, chunkSize, progressPipe):
 
         data=b""
-        l=chunkSize//1000
+        bytePerRecv=chunkSize//1000
 
-        if chunkSize < l:
-            data=data + recvByte(connection, chunkSize)
-        else:
-            temp=0
-            while temp< chunkSize:
-                last = temp + l
-                if last < chunkSize:
-                    data=data+recvByte(connection, l)
-                else:
-                    data=data+recvByte(connection, chunkSize % l)
+        temp=0
+        while temp < chunkSize:
+            last = temp + bytePerRecv
+            if last < chunkSize:
+                data=data+recvByte(connection, bytePerRecv)
+            else:
+                data=data+recvByte(connection, chunkSize % bytePerRecv)
 
-                temp = len(data)
-                progress=floor(temp/chunkSize*100.0)
-                processPipe.send(progress)
+            temp = len(data)
+            progressPipe.send(floor(temp/chunkSize*100.0))
 
 
-        pipe.send(data)
+        dataPipe.send(data)
 
 
 
+def printProgress(pipe_1, pipe_2, pipe_3, pipe_4, fileName):
 
+    msg_1 = msg_2 = msg_3 = msg_4 = 0
 
-def printProcess(a,b,c,d,pipe1,pipe2,pipe3,pipe4,fileName):
+    while True:
+        if pipe_1.poll(): msg_1 = pipe_1.recv()
+        if pipe_2.poll(): msg_2 = pipe_2.recv()
+        if pipe_3.poll(): msg_3 = pipe_3.recv()
+        if pipe_4.poll(): msg_4 = pipe_4.recv()
 
+        print(f"\r[INFO] Downloading {fileName} | "
+            f"Part 1: {msg_1}% | "
+            f"Part 2: {msg_2}% | "
+            f"Part 3: {msg_3}% | "
+            f"Part 4: {msg_4}%   ",end="")
 
-    msg1=0
-    msg2=0
-    msg3=0
-    msg4=0
-
-    while a.is_alive() or b.is_alive() or c.is_alive() or d.is_alive():
-        if pipe1.poll():
-            msg1 = pipe1.recv()
-        if pipe2.poll():
-            msg2 = pipe2.recv()
-        if pipe3.poll():
-            msg3 = pipe3.recv()
-        if pipe4.poll():
-            msg4 = pipe4.recv()
-
-        # Sử dụng \r để in lại 4 dòng cố định
-        print(
-            f"\r[INFO] Downloading {fileName} | "
-            f"Part 1: {msg1}% | "
-            f"Part 2: {msg2}% | "
-            f"Part 3: {msg3}% | "
-            f"Part 4: {msg4}%    ",end="")
-
-
-        if msg1>=100 and msg2>=100 and msg3>=100 and msg4>=100:
+        if msg_1>=100 and msg_2>=100 and msg_3>=100 and msg_4>=100:
             break
 
 
 def start_client(serverIP,serverPort,folder):
 
+    # Tao 4 socket va connect toi server
     receiver = [socket.socket(socket.AF_INET, socket.SOCK_STREAM) for _ in range(4)]
 
     server_address = (serverIP, serverPort)
-    print("[INFO]Client connect to server with port: " + str(serverPort))
+    print("[INFO] Client connect to server: "+str(server_address))
 
     for clientSocket in receiver:
         clientSocket.connect(server_address)
 
     try:
 
-        size = recvNumber(receiver[0], 1024)
-        fileList = recvString(receiver[0], size)
-        print(f"[INFO]Danh sach cac file co the download la:\n{fileList}\n")
+        fileListSize = recvNumber(receiver[0], 1024)
+        fileList = recvString(receiver[0], fileListSize)
+        print(f"[INFO] Danh sach cac file co the download la:\n{fileList}\n")
 
-        start_time = time.time()
+
         oldSize = getFileSize(folder+"input.txt")
         downloaded=[]
+        isDownloaded = True
 
 
         while True:
-            if isChange(folder+"input.txt", oldSize):
 
-                newSize = getFileSize(folder+"input.txt")
-                changes = split_string(fileDataFrom(folder+"input.txt", oldSize), '\n')
+            sizeChanged=countChangedCharacters(folder + "input.txt", oldSize)
 
-                for i in range(len(changes)):
-                    flag=True
-                    if changes[i] in downloaded:
-                        flag=False
-                        print(f"[INFO] File {changes[i]} da duoc download")
-                    if changes[i] != "" and flag :
-                        downloaded.append(changes[i])
-                        sendNumber(len(changes[i]), receiver[0])  # Gui do dai ten file
-                        sendString(changes[i], receiver[0])  # Gui ten file
+            if sizeChanged>0:
+
+                fileData=getFileData(folder+"input.txt")
+                fileNames = splitString(fileData[-sizeChanged:], '\n')
+
+                for i in range(len(fileNames)):
+
+                    if fileNames[i] in downloaded:
+                        isDownloaded=False
+                        print(f"[INFO] File {fileNames[i]} da duoc download")
+
+
+                    if  isDownloaded :
+                        downloaded.append(fileNames[i])
+                        sendNumber(len(fileNames[i]), receiver[0])
+                        sendString(fileNames[i], receiver[0])
 
                         # Nhận kích thước file
+                        fileSize = recvNumber(receiver[0],1024)
 
-                        a=receiver[0].recv(1024).decode()
-                        print(a)
-                        filesize = int(a)
-                        if filesize!=-1:
+                        if fileSize!=-1:  # Truong hop file khong co tren server
 
-                            chunkSize = filesize // 4
-                            lock = Lock() 
-                            output_1, input_1 = Pipe()
-                            output_2, input_2 = Pipe()
-                            output_3, input_3 = Pipe()
-                            output_4, input_4 = Pipe()
+                            chunkSize = fileSize // 4
 
-                            progress1, p1=Pipe()
-                            progress2, p2=Pipe()
-                            progress3, p3=Pipe()
-                            progress4, p4=Pipe()
+                            # Tao cac Pipe de giao tiep giua cac process
+                            pipes = [Pipe() for _ in range(8)]
+                            outputs, inputs = zip(*pipes)
+                            output_1, output_2, output_3, output_4,progress_1, progress_2,progress_3, progress_4= outputs
+                            input_1, input_2, input_3, input_4, p1, p2, p3, p4 = inputs
 
 
+                            # Tao cac ham nhan song song
                             receiver1 = Process(target=receiveChunk,
-                                                               args=(input_1,receiver[0], chunkSize,1,changes[i],p1))
+                                                               args=(input_1,receiver[0], chunkSize,p1))
                             receiver2 = Process(target=receiveChunk,
-                                                                args=(input_2, receiver[1], chunkSize, 2, changes[i],p2))
+                                                                args=(input_2, receiver[1], chunkSize,p2))
                             receiver3 = Process(target=receiveChunk,
-                                                                args=(input_3, receiver[2], chunkSize, 3, changes[i],p3))
+                                                                args=(input_3, receiver[2], chunkSize,p3))
                             receiver4 = Process(target=receiveChunk,
-                                                                args=(input_4, receiver[3], filesize-3*chunkSize, 4, changes[i],p4))
+                                                                args=(input_4, receiver[3], fileSize-3*chunkSize,p4))
+
                             processes = [receiver1,receiver2,receiver3,receiver4]
-
-
 
                             for p in processes:
                                 p.start()
 
-                            printProcess(receiver1,receiver2,receiver3,receiver4,progress1,progress2,progress3,progress4,changes[i])
-                            output=[output_1.recv(),output_2.recv(),output_3.recv(),output_4.recv()]
+                            printProgress(progress_1, progress_2, progress_3, progress_4, fileNames[i]) # In phan tram
+                            output=[output_1.recv(),output_2.recv(),output_3.recv(),output_4.recv()] # Data nhan duoc
+
                             for p in processes:
                                 p.join()
 
-                            print(f"\n[DEBUG] Starting to write file {changes[i]}")
-                            with open(folder+changes[i], "wb") as f:
+                            print(f"\n[DEBUG] Starting to write file {fileNames[i]}")
+                            with open(folder+fileNames[i], "wb") as f:
                                 for data in output:
                                     f.write(data)
-                                print(f"[INFO] Da ghi file {changes[i]}")
+                                print(f"[INFO] Da ghi file {fileNames[i]}")
+
                         else:
-                            print(f"[INFO] File {changes[i]} khong ton tai tren server")
-                oldSize = newSize
-            else:
-                oldSize = getFileSize(folder+"input.txt")
+                            print(f"[INFO] File {fileNames[i]} khong ton tai tren server")
+
             time.sleep(5)
 
 
@@ -216,4 +202,4 @@ def start_client(serverIP,serverPort,folder):
             clientSocket.close()
 
 if __name__ == "__main__":
-    start_client("27.27.25.210",65432,"Client/")
+    start_client("127.0.0.1",65432,"Client/")
